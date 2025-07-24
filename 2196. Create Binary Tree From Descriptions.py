3a0b394cc9a22b99bc6e751c50d0fc313d932bdc
Mazie <f.vafadoost@gmail.com>
2025-07-23 19:57:43 -0700

    solution: 2196. Create Binary Tree From Descriptions

---
+++ 2196. Create Binary Tree From Descriptions.py
@@ -0,0 +1,35 @@
+"""You are given a 2D integer array descriptions where descriptions[i] = [parenti, childi, isLefti] indicates that parenti is the parent of childi in a binary tree of unique values. Furthermore,
+
+If isLefti == 1, then childi is the left child of parenti.
+If isLefti == 0, then childi is the right child of parenti.
+Construct the binary tree described by descriptions and return its root.
+
+The test cases will be generated such that the binary tree is valid."""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
+        nodes = {}
+        is_child = set()
+
+        for parent_val, child_val, is_left in descriptions:
+            if parent_val not in nodes:
+                nodes[parent_val] = TreeNode(parent_val)
+            if child_val not in nodes:
+                nodes[child_val] = TreeNode(child_val)
+            if is_left:
+                nodes[parent_val].left = nodes[child_val]
+            else:
+                nodes[parent_val].right = nodes[child_val]
+
+            is_child.add(child_val)
+
+        for node_val in nodes:
+            if node_val not in is_child:
+                return nodes[node_val]
